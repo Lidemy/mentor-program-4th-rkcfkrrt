fbc95b1234742912a3586a14bb76997597fe45fc
rkcfkrrt <lwy12993@gmail.com>
2020-06-27 20:34:53 +0800

    完成 hw1、hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,7 @@
 function capitalize(str) {
+    var a =str.charAt(0)
+    var b =str.replace(a, a.toUpperCase()) 
+    return b
+  }
   
-}
-
-console.log(capitalize('hello'));
+  console.log(capitalize('hello'));
